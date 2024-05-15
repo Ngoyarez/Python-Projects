ec51b61d26f9678759f72cf016e97afd223d809c
Ngoyarez <brianngoya764@yahoo.com>
2024-05-15 16:42:30 +0300

    Multiplication Table Generator

---
+++ multiplicationtable.py
@@ -0,0 +1,24 @@
+"""Multiplication Table, by Brian Ngoya
+Print a multiplication table.
+"""
+
+print('Multiplication Table, by Brian Ngoya')
+
+# Print the horizontal number labels:
+print('  |  0   1   2   3   4   5   6   7   8   9  10  11  12')
+print('--+---------------------------------------------------')
+
+# Display each row of products:
+for number1 in range(0, 13):
+
+    # Print the vertical numbers labels:
+    print(str(number1).rjust(2), end='')
+
+    # Print a separating bar:
+    print('|', end='')
+
+    for number2 in range(0, 13):
+        # Print the product followed by a space:
+        print(str(number1 * number2).rjust(3), end=' ')
+
+    print()  # Finish the row by printing a newline.
